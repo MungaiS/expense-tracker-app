abf1bfe7424c6280871ae02b6d943d10daa24ee4
MungaiS <129229232+MungaiS@users.noreply.github.com>
2024-01-19 10:42:05 +0300

    Added Submit event Listener

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addTransactionDOM(transaction) {
   item.classList.add(transaction.amount > 0 ? "plus" : "minus");
 
   item.innerHTML = `<h4>${transaction.text}</h4> <span>${sign}$${Math.abs(transaction.amount)}<i class="fa-solid fa-caret-${icon}"></i>
-  <i class="fa-solid fa-trash-can"></i></span>`;
+  <i class="fa-solid fa-trash-can" onclick="removeItem(${transaction.id})"></i></span>`;
 
   list.appendChild(item);
 }
@@ -27,81 +27,59 @@ function addTransactionDOM(transaction) {
 function updateValue(){
   const amount = transactions.map(item => item.amount)
 
-  const total = amount.reduce((a, b) => (a +=b), 0).toFixed(2);
+  const total = amount
+  .reduce((a, b) => (a += b), 0)
+  .toFixed(2);
 
-  const income = amount.filter(item => item > 0).reduce ((a,b) => (a +=b),0).toFixed(2);
+  const income = amount
+  .filter(item => item > 0)
+  .reduce ((a, b) => (a += b), 0)
+  .toFixed(2);
 
-  const expense = amount.filter(item => item < 0).reduce ((a,b) => (a +=b),0).toFixed(2);
+  const expense = amount
+  .filter(item => item < 0)
+  .reduce ((a, b) => (a += b), 0)
+  .toFixed(2);
 
   balance.innerHTML = `$${total}`;
 
   moneyPlus.innerHTML = `$${income} <i class="fas fa-caret-up"></i>`;
 
-  moneyMinus.innerHTML = `$${Math.abs(expense).toFixed(2)} <i class="fa-solid fa-trash-can delete" onclick="removeItem(${transaction.id})></i>`;
+  moneyMinus.innerHTML = `$${Math.abs(expense).toFixed(2)} <i class="fas fa-caret-down"></i>`;
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  
-  if (text.value.trim() === '' || amount.value.trim() === ""){
-    alert("'Please fill in the data first")
-  } else {
-    const transaction = {id:randomId(), text:text.value, amount:parseInt(amount.value)};
+function randomId() {
+  return Math.floor((Math.random() * 1000))
+}
 
+form.addEventListener("submit", (e) =>{
+  e.preventDefault()
+
+  if (text.ariaValueMax.trim() === '' || amount.value.trim() === '') {
+    alert("Please fill your data first");
+  } else {
+    const transaction = {
+      id:randomId(), 
+      text:text.value, 
+      amount:parseInt(amount.value)
+    };
     transactions.push(transaction);
 
     addTransactionDOM(transaction);
 
     updateValue();
 
-    updateLocalStorage();
-
     text.value = "";
     amount.value = "";
+
   }
-  
 });
 
-document.getElementById("addTransactionBtn").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const transaction = { id: randomId(), text: text.value, amount: parseInt(amount.value) };
-
-  transactions.push(transaction);
-
-  addTransactionDOM(transaction);
-
-  updateValue();
-
-  updateLocalStorage();
-
-  text.value = "";
-  amount.value = "";
-});
-
-// Add EventListener to delete transaction
-document.querySelectorAll(".fa-trash-can").forEach((icon) => {
-  icon.addEventListener("click", function () {
-    const transactionId = parseInt(icon.parentNode.parentNode.getAttribute("data-id"));
-    removeItem(transactionId);
-  });
-});
-
-function randomId() {
-  return Math.floor((Math.random() * 1000 ));
-}
-
-
-function removeItem(id){
+function removeItem(id) {
   transactions = transactions.filter(transaction => transaction.id !== id)
 
   init()
 }
-
-function updateLocalStorage(){
-  localStorage.setItem("transactions", JSON.stringify(transactions));
-}
-
 
 // init 
 
